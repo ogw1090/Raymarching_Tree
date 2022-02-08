55417cf029eaffe14df397b56e52412ae5413598
ogw_desktop <hoge@hoge.com>
2022-02-08 09:02:32 +0900

    Edit RaymarchingTreeTwist.shadergraph

---
+++ Assets/Scripts/TwistObject.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TwistObject : MonoBehaviour
+{
+    private MeshRenderer m;
+    private Vector3 mouse;
+    float ini_posx;
+    float dist = 0;
+    int count = 0;
+    const float MAX_RANGE = 30;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m = this.GetComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mouse.x = MAX_RANGE * (Input.mousePosition.x - Screen.width / 2) / Screen.width;
+
+        if (Input.GetMouseButton(0)) //マウスがクリックされたら
+        {
+            if (count == 0)
+            {
+                ini_posx = mouse.x;
+            }
+            dist = mouse.x - ini_posx;
+            m.material.SetFloat("Vector1_a52cf50a8f9a419485bec3890ad162a4", Mathf.Clamp(dist, -MAX_RANGE, MAX_RANGE));
+            count++;
+            Debug.Log(mouse.x);
+        }
+
+        if (Input.GetMouseButtonUp(0)) //マウスが離されたら
+        {
+            StartCoroutine(SincCurve(dist));
+            count = 0;
+            dist = 0;
+        }
+
+        if (Input.GetMouseButtonDown(1)) //右クリック
+        {
+            Debug.Log("aa");
+        }
+
+
+    }
+
+    IEnumerator SincCurve(float d)
+    {
+        float x = 0;
+        float ymax = d;
+        while (true)
+        {
+
+            float b = ymax * Mathf.Sin(x) / (2 * x) ;
+            m.material.SetFloat("Vector1_a52cf50a8f9a419485bec3890ad162a4", b);
+
+            x += 0.5f;
+            yield return new WaitForSeconds(0.01f);
+
+            if (x > 100f)
+            {
+                yield break;
+            }
+
+        }
+
+    }
+}
--- Assets/Shader/HLSL/RayMarchingTreeTwist.hlsl
+++ Assets/Shader/HLSL/RayMarchingTreeTwist.hlsl
@@ -2,13 +2,15 @@
 //質点の距離関数"length(p)"から半径を引くと，球の距離関数が定義できる
 //float PI = 3.14159;
 float3 offset;
+float k;
 
 //Capsule Line
 float ln(float3 p, float3 a, float3 b, float R) 
 {
     float r = dot(p - a, b - a) / dot(b - a, b - a);
     r = clamp(r, 0., 1.);
-    p.x += 0.2 * sqrt(R) * smoothstep(1., 0., abs(r * 2. - 1.)) * cos(PI * (2. * _Time.y));
+    //p.x += 0.2 * sqrt(R) * smoothstep(1., 0., abs(r * 2. - 1.)) * cos(PI * (2. * _Time.y));
+    p.x += 0.2 * sqrt(R) * smoothstep(1., 0., abs(r * 2. - 1.));
     return length(p - a - (b - a) * r) - R * (1.5 - 0.4 * r);
 }
 
@@ -18,20 +20,37 @@ float2x2 ro(float a)
     return float2x2(c, -s, s, c);
 }
 
+
 float Dist(float3 p) 
 {
+
+    //float c = cos(k * p.y);
+    //float s = sin(k * p.y);
+    //float2x2 m = float2x2(c, -s, s, c);
+    //float2 q = mul(m, p.xz);
+    //p.x = q.x;
+    //p.z = q.y;
+
     p -= offset;
     //rl: 枝のパラメータ
     //rl(枝半径、終点)
-    float2 rl = float2(0.02, .25 + 0.01 * sin(PI * 4. * _Time.y));
-    //float2 rl = float2(0.02, 0.25);
+    //float2 rl = float2(0.02, .25 + 0.01 * sin(PI * 4. * _Time.y));
+    float2 rl = float2(0.02, 0.25);
     float l = length(p) - 1e-2;
 
-    //p.zx *= ro(.5 * _Time.y);
-    p.zx = mul(p.zx, ro(.5 * _Time.y));
+    //p.zx = mul(p.zx, ro(.5 * _Time.y));
 
     for (int i = 1; i < 9; i++)
     {
+        if (i <= 2)
+        {
+            float c = cos(k * p.y);
+            float s = sin(k * p.y);
+            float2x2 m = float2x2(c, -s, s, c);
+            float2 q = mul(m, p.xz);
+            p.x = q.x;
+            p.z = q.y;
+        }
         float a = 0.6 + 0.4 * sin(_Time.y) * sin(0.871 * _Time.y) + 0.05 * float(i) * sin(2. * _Time.y);
         float b = 0.0;
         float c = 0.5 * PI + 0.2 * sin(0.5278 * _Time.y) + 0.8 * float(i) * (sin(0.1 * _Time.y) * (sin(0.1 * PI * _Time.y) + sin(0.333 * _Time.y) + 0.2 * sin(1.292 * _Time.y)));
@@ -39,52 +58,18 @@ float Dist(float3 p)
         l = min(l, ln(p, float3(0, 0, 0), float3(0, rl.y, 0), rl.x));
         p.y -= rl.y;
         p.x = abs(p.x);
-        p.xy = mul(p.xy, ro(-a));
         //p.xy = mul(p.xy, ro(-a));
-        //p.zy = mul(p.zy, ro(-0.3));
+        p.xy = mul(p.xy, ro(-0.7));
+        //p.zx = mul(p.zx, ro(0.3 * float(i)));
         p.zx = mul(p.zx, ro(c));
         l = min(l, length(p) - 0.15 * sqrt(rl.x));
-        rl *= (0.7 + 0.015 * float(i) * (sin(_Time.y) + 0.1 * sin(4. * PI * _Time.y)));
-        //rl *= 0.7;
+        if ( i == 8)
+        {
+            l = min(l, length(p) - 0.01 * sin(0.1 * PI * _Time.y));
+        }
+        //rl *= (0.7 + 0.015 * float(i) * (sin(_Time.y) + 0.1 * sin(4. * PI * _Time.y)));
+        rl *= 0.65;
     }
-
-
-
-    //for (int i = 1; i < 5; i++) {
-
-    //    l = min(l, ln(p, float3(0, 0, 0), float3(0, rl.y, 0), rl.x));
-
-    //    //ToDo
-
-    //    //p.y -= rl.y + offset;
-
-
-
-
-    //    //p.xy = mul(p.xy, ro(0.2 * sin(3.1 * _Time.y + float(i)) + sin(0.222 * _Time.y) * (-0.1 * sin(0.4 * PI * _Time.y) + sin(0.543 * _Time.y) / max(float(i), 2.))));
-    //    //p.xy = mul(p.xy, ro(0.2 * sin(3.1 * _Time.y + float(i)) + sin(0.222 * _Time.y) * (-0.1 * sin(0.4 * PI * _Time.y) + sin(0.543 * _Time.y) / max(float(i), 2.))));
-    //    //p.xy = mul(p.xy, ro(0.1 * float(i)));
-    //    p.xy = mul(p.xy, ro(0.7));
-
-    //    p.x = abs(p.x);
-    //    
-    //    //p.xy = mul(p.xy, ro(0.6 + 0.4 * sin(_Time.y) * sin(0.871 * _Time.y) + 0.05 * float(i) * sin(2. * _Time.y)));
-    //    //p.xy = mul(p.xy, ro(0.6 + 0.4 * sin(_Time.y) * sin(0.871 * _Time.y) + 0.05 * float(i) * sin(2. * _Time.y)));
-    //    //p.xy = mul(p.xy, ro(0.3 * float(i)));
-    //    //p.xy = mul(p.xy, ro(0.7));
-
-    //    //p.zx = mul(p.zx, ro(0.5 * PI + 0.2 * sin(0.5278 * _Time.y) + 0.8 * float(i) * (sin(0.1 * _Time.y) * (sin(0.1 * PI * _Time.y) + sin(0.333 * _Time.y) + 0.2 * sin(1.292 * _Time.y)))));
-    //    //
-
-    //    //rl *= (0.7 + 0.015 * float(i) * (sin(_Time.y) + 0.1 * sin(4. * PI * _Time.y)));
-
-    //    p.y -= rl.y + offset;
-
-    //    rl *= 0.5;
-    //    //min(枝,　節)
-    //    //l = min(l, length(p) - 0.15 * sqrt(rl.x));
-    //    l = min(l, length(p) - 0.01);
-    //}
 
     return l;
 }
@@ -105,10 +90,12 @@ float3 CalcNormal(float3 p)
 }
 
 //マーチングループの本体
-void RayMarchingTree_float(
+void RayMarchingTreeTwist_float
+(
     float3 RayPosition,
     float3 RayDirection,
     float3 Offset,
+    float Twist_k,
     out bool Hit,
     out float3 HitPosition,
     out float3 HitNormal
@@ -116,6 +103,7 @@ void RayMarchingTree_float(
 {
     float3 pos = RayPosition;
     offset = Offset;
+    k = Twist_k;
 
     //各ピクセルごとに64回のループをまわす
     for(int i = 0; i < 24; i ++)
